9600b82d4cee35ed851dc346e8fd1d9923b4f540
Emmanuel Neni <emmanuelneni@gmail.com>
2020-09-02 22:03:35 +0300

    fixing random number bug

--- quote-machine-vanilla/index.js
+++ quote-machine-vanilla/index.js
@@ -5,10 +5,18 @@ const tweetQuoteBtn = document.querySelector('#tweet-quote')
 let currentQuote
 
 const getRandomQuote = () => {
-    return quotes[Math.floor(Math.random() * quotes.length - 1)];
+    return quotes[Math.floor((Math.random() * quotes.length - 1) + 1)];
 }
 
 currentQuote = getRandomQuote()
-
 quoteText.innerHTML = currentQuote.quoteText
 authorText.innerHTML = currentQuote.quoteAuthor
+tweetQuoteBtn.setAttribute('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${currentQuote.quoteText}%20\n${currentQuote.quoteAuthor}`)
+
+newQuoteBtn.onclick = () => {
+    currentQuote = getRandomQuote()
+    console.log(currentQuote);
+    quoteText.innerHTML = currentQuote.quoteText
+    authorText.innerHTML = currentQuote.quoteAuthor
+    tweetQuoteBtn.setAttribute('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${currentQuote.quoteText}%20\n${currentQuote.quoteAuthor}`)
+}
