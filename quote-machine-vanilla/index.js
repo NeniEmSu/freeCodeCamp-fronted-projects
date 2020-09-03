6824a069f6e6bf6be4fc75fe01ce5c1b25c8bd00
Emmanuel Neni <emmanuelneni@gmail.com>
2020-09-03 09:53:08 +0300

    moved functions to fetch and using fetched data for display

--- quote-machine-vanilla/index.js
+++ quote-machine-vanilla/index.js
@@ -3,24 +3,26 @@ const authorText = document.querySelector('#author')
 const newQuoteBtn = document.querySelector('#new-quote')
 const tweetQuoteBtn = document.querySelector('#tweet-quote')
 let currentQuote
+let fetchedQuotes
 
 fetch('https://raw.githubusercontent.com/JamesFT/Database-Quotes-JSON/master/quotes.json')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => fetchedQuotes = data)
+    .then(() => {
+        const getRandomQuote = () => {
+            return fetchedQuotes[Math.floor((Math.random() * fetchedQuotes.length - 1) + 1)];
+        }
+        
+        currentQuote = getRandomQuote()
+        quoteText.innerHTML = currentQuote.quoteText
+        authorText.innerHTML = currentQuote.quoteAuthor ? `- ${currentQuote.quoteAuthor}` : '-Anonymous!'
+        tweetQuoteBtn.setAttribute('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${currentQuote.quoteText}%20\n${currentQuote.quoteAuthor}`)
+        
+        newQuoteBtn.onclick = () => {
+            currentQuote = getRandomQuote()
+            quoteText.innerHTML = currentQuote.quoteText
+            authorText.innerHTML = currentQuote.quoteAuthor ? `- ${currentQuote.quoteAuthor}` : '-Anonymous!'
+            tweetQuoteBtn.setAttribute('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${currentQuote.quoteText}%20\n${currentQuote.quoteAuthor}`)
+        }
+    })
     .catch(error => console.error(error))
-
-const getRandomQuote = () => {
-    return quotes[Math.floor((Math.random() * quotes.length - 1) + 1)];
-}
-
-currentQuote = getRandomQuote()
-quoteText.innerHTML = currentQuote.quoteText
-authorText.innerHTML = currentQuote.quoteAuthor
-tweetQuoteBtn.setAttribute('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${currentQuote.quoteText}%20\n${currentQuote.quoteAuthor}`)
-
-newQuoteBtn.onclick = () => {
-    currentQuote = getRandomQuote()
-    quoteText.innerHTML = currentQuote.quoteText
-    authorText.innerHTML = currentQuote.quoteAuthor
-    tweetQuoteBtn.setAttribute('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${currentQuote.quoteText}%20\n${currentQuote.quoteAuthor}`)
-}
