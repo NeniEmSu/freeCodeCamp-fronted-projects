94ad0859b85f27c866c008190dfa597ea0a4e625
Emmanuel Neni <emmanuelneni@gmail.com>
2020-09-05 23:21:00 +0300

    importing and using bootstrap, polyfill and vue markdown

--- markdown-previewr-vue/src/main.js
+++ markdown-previewr-vue/src/main.js
@@ -1,8 +1,16 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+
+import VueMarkdown from 'vue-markdown'
 
 Vue.config.productionTip = false
 
 new Vue({
+  components: {
+    VueMarkdown
+  },
   render: h => h(App),
 }).$mount('#app')
